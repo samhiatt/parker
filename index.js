88977647dd2ced65fbb121d043068785a34a76ff
Sam Hiatt <sam.hiatt@gmail.com>
2015-09-12 05:16:36 -0700

    HTTP interface with Express

--- index.js
+++ index.js
@@ -3,6 +3,44 @@
  */
 var postgeo = require("postgeo");
 var proj4 = require("proj4");
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	if (!req.query.lat || !req.query.lon) {
+		res.status(400).send("Expected 'lat' and 'lon' query params.");
+		return;
+	}
+	
+	var point = transform.forward([parseFloat(req.query.lon),parseFloat(req.query.lat)]);
+	//var point = transform.forward([-122.444,37.72]);
+	console.log("Request for point: ",point);
+	
+	var queryString = "SELECT gid, weekday, blockside, cnnrightle, fromhour, tohour, streetname"+
+		", ST_AsGeoJson(ST_Transform(geom,4326)) AS geometry"+
+		", ST_Distance(geom,ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227)) AS distance "+
+		", ST_AsText(ST_ClosestPoint(ST_GeometryN(geom,1), ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227))) AS closest_point "+
+		"FROM sfsweeproutes WHERE ST_DWithin(geom,ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227),300)"+
+		"ORDER BY distance LIMIT 6;";
+
+	console.log(queryString);
+
+	postgeo.query(queryString,
+		"geojson", function(data) {
+			//console.log(JSON.stringify(data));
+			console.log(data.features||data);
+			res.send(JSON.stringify(data));
+		});
+});
+
+postgeo.connect("postgres://localhost/sam");
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
 
 proj4.defs([
 	[
@@ -12,21 +50,3 @@ proj4.defs([
 ]);
 
 var transform = proj4('EPSG:2227');
-
-var point = transform.forward([-122.444,37.72]);
-
-postgeo.connect("postgres://localhost/sam");
-
-var queryString = "SELECT gid, weekday, blockside, cnnrightle, fromhour, tohour, streetname"+
-	", ST_AsGeoJSON(geom) AS geometry, ST_Distance(geom,ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227)) AS distance "+
-	", ST_AsText(ST_ClosestPoint(ST_GeometryN(geom,1), ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227))) AS closest_point "+
-	"FROM sfsweeproutes WHERE ST_DWithin(geom,ST_GeomFromText('POINT("+point[0]+" "+point[1]+")',2227),300)"+
-	"ORDER BY distance LIMIT 6;";
-
-console.log(queryString);
-
-postgeo.query(queryString, 
-	"geojson", function(data) {
-	console.log(JSON.stringify(data));
-	console.log(data.features||data)
-});
